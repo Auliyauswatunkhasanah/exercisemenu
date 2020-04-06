8516104ff57a12a106775920eb8d4fef1de75592
Auliyauswatunkhasanah <auliyauswatunn@gmail.com>
2020-04-06 23:40:48 +0700

    first commit

---
+++ app/src/main/java/com/example/exercisemenu18a/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.exercisemenu18a;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button btnok;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.editNa);
+        password = (EditText) findViewById(R.id.editAl);
+        btnok = (Button) findViewById(R.id.btnok);
+
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameKey = username.getText().toString();
+                String passwordKey = password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")) {
+
+                    Toast.makeText(getApplicationContext(),"Login Sukses", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, pagemenuActivity.class);
+                    MainActivity.this.startActivity(intent);
+                }else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password anda salah!!!").setNegativeButton("Retry", null).create().show();
+
+                }
+            }
+        });
+    }
+}
